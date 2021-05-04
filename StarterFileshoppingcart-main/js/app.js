d80155cef542757a0b2baea48df1719bd583791e
MarjorieRodriguez <rodzmarjmaj@gmail.com>
2021-05-04 11:04:02 +0800

    First Steps with the project

---
+++ StarterFileshoppingcart-main/js/app.js
@@ -0,0 +1,4 @@
+// Variables
+const courses = document.querySelector('#courses-list'),
+      shoppingCartContent = document.querySelector('#cart-content tbody'),
+      clearCartBtn = document.querySelector('#clear-cart');
