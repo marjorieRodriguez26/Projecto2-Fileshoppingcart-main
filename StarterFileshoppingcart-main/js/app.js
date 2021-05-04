9efad59e41bfdcec52569bd182a580d14f547eb8
MarjorieRodriguez <rodzmarjmaj@gmail.com>
2021-05-04 11:05:43 +0800

    Adding courses into the shopping Cart part 01

--- StarterFileshoppingcart-main/js/app.js
+++ StarterFileshoppingcart-main/js/app.js
@@ -2,3 +2,20 @@
 const courses = document.querySelector('#courses-list'),
       shoppingCartContent = document.querySelector('#cart-content tbody'),
       clearCartBtn = document.querySelector('#clear-cart');
+// Listeners
+
+loadEventListeners();
+
+function loadEventListeners() {
+    // when a new course is added
+    courses.addEventListener('click', buyCourse);
+
+    // When the remove button is Clicked
+    shoppingCartContent.addEventListener('click', removeCourse);
+
+    // Clear Cart Btn
+    clearCartBtn.addEventListener('click', clearCart);
+
+    // document ready
+    document.addEventListener('DOMContentLoaded', getFromLocalStorage);
+}
